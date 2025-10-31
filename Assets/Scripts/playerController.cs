f62dd70dcae45f95dfeb7663cb8fd134eb3039f8
DmitryY <D23125246@mytudublin.ie>
2025-10-31 18:15:57 +0000

    Controller set up, tweaks needed

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,12 +6,16 @@ public class playerController : MonoBehaviour
 {
     public InputActionReference TiltAction, Wings, left, right;
     // public InputActionReference Wings;
-    public float TiltSpeed, WingsSpeed = 100f;
+    public float TiltSpeed, WingsSpeed, jumpForce, jumpAngleZ;
     public float tiltValue, WingsValue;
     [SerializeField] private bool leftPressed, rightPressed, bothPressed;
-    
-    
-    
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
 
@@ -20,6 +24,13 @@ public class playerController : MonoBehaviour
         if (!bothPressed)
         {
             transform.Rotate(Vector3.forward, WingsValue * WingsSpeed * Time.deltaTime);
+        }
+
+        if (bothPressed)
+        {
+            Vector3 localDirection = new Vector3(0, 1, jumpAngleZ).normalized;
+            Vector3 WorldDirection = transform.TransformDirection(localDirection);
+            rb.AddForce(WorldDirection * jumpForce, ForceMode.Impulse);
         }
 
         WingsValue = Wings.action.ReadValue<float>();
